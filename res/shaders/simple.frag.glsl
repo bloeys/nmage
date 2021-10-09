7a29b411ac04be6623cd34f7d44abf4549ba63cc
bloeys <bloeys@outlook.com>
2021-10-09 21:54:38 +0400

    Fresh start

--- res/shaders/simple.frag.glsl
+++
@@ -1,9 +0,0 @@
-#version 460 core
-
-uniform vec3 c;
-
-out vec4 fragColor;
-
-void main() {
-    fragColor = vec4(c, 1);
-}
--- res/shaders/simple.vert.glsl
+++
@@ -1,7 +0,0 @@
-#version 460 core
-
-in vec3 vertPos;
-
-void main() {
-    gl_Position = vec4(vertPos, 1.0);
-}
